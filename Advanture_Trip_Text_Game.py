2262667ea11370074376bfe778cabcbe3ad584b9
Akshaykumar Patel <patel.ak@husky.neu.edu>
2018-09-20 02:00:27 +0530

    First draft of Advanture Trip Text Game - Updated By Akshay

---
+++ Advanture_Trip_Text_Game.py
@@ -0,0 +1,42 @@
+# Advanture Trip Text Game
+
+
+print("------------------------------------------------------------")
+print("------------------------------------------------------------")
+print("Game Start")
+print("------------------------------------------------------------")
+print("------------------------------------------------------------")
+print("Put Description of the advanture Trip Text Game")
+print("Print rule and condition for the game")
+print("Print winning condition for the game")
+print("Print the health of the player")
+
+
+# All user defined function writtern below
+def handle_input():       # Function to handle the Input which checks the quality of the Input
+    print("hello")
+
+
+def get_input(current_step_temp):          # Function to get the Input options for the player for each states comes during the game
+    user_options ={0: [],                  #
+                   1: [],
+                   2: [],
+                   3: [],
+                   4: [],
+                   5: []}
+    stage_option = user_options[current_step_temp]
+
+def track_player_step():  # Function to save all the steps which player will take during the game.
+    print("hello")
+
+
+# main code start from here
+
+current_step = 0          # variable to store the value of current step which player will take during the game.
+player_health = 16        # player health variable at the start of game.
+
+while player_health <= 0: # main loop to check the lifeline of the player during the game
+    get_input(current_step)
+
+
+
